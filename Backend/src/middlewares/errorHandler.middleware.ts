68bab26be4b1163f995f3eaac5b8b191a0975dd0
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2024-07-22 13:14:33 +0545

    add interpolate util and basic messages util

--- Backend/src/middlewares/errorHandler.middleware.ts
+++ Backend/src/middlewares/errorHandler.middleware.ts
@@ -1,12 +1,12 @@
 import { NextFunction, Request, Response } from "express";
-
+import HttpStatusCodes from "http-status-codes";
 export const genericErrorHandler = (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-
+  return res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).json({
     
-    
+  });
 };
---
+++ Backend/src/utils/message.ts
@@ -0,0 +1,13 @@
+export const successMessages = {
+  FETCHED: ":item fetched successfully.",
+  CREATED: ":item created successfully.",
+  UPDATED: ":item updated successfully.",
+  DELETED: ":item deleted successfully.",
+};
+
+export const errorMessages = {
+  NOTFOUND: ":item not found.",
+  INTERNAL_SERVER_ERROR: "Internal Server Error.",
+  UNAUTHORIZED_ACCESS: "Unauthorized Access.",
+  BAD_REQUEST: "Bad request",
+};
