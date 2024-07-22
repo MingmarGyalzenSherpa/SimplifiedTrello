35f25c1129429eecc61a38a3db5ebfb985218f57
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2024-07-22 12:50:35 +0545

    add custom error classes

---
+++ Backend/src/errors/UnauthorizedError.ts
@@ -0,0 +1,3 @@
+import { BaseError } from "./baseError";
+
+export class UnauthorizedError extends BaseError {}
---
+++ Backend/src/middlewares/errorHandler.middleware.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+
+export const genericErrorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+
+    
+    
+};
