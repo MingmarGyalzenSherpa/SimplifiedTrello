7a1f3d4743b2a378d0d41bcd2b694e3aae75502f
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2024-07-29 22:28:14 +0545

    fix list css

--- Frontend/src/components/Card.ts
+++ Frontend/src/components/Card.ts
@@ -1,4 +1,18 @@
-export function Card(parentEl: HTMLElement) {
-  const cardEl = document.createElement("div");
-}
+export class Card {
+  state: object;
+  elements: {
+    parentEl: HTMLElement;
+  };
+  constructor(parentEl: HTMLElement) {
+    this.state = {};
+    this.elements = {
+      parentEl,
+    };
+  }
 
+  initialSetup = () => {};
+
+  setupEventListener = () => {};
+
+  render = () => {};
+}
--- Frontend/src/components/List.ts
+++ Frontend/src/components/List.ts
@@ -29,31 +29,34 @@ export class List {
     console.log(this.elements.parentEl);
     const listEl = document.createElement("div");
     listEl.innerHTML += `
-    <div id="list" class="bg-blue-gray-500 min-w-[300px] max-h-[600px]  rounded-3xl overflow-auto">
-          <span class="block list-title mb-3 pt-3 px-4 "> ${this.state.list.title}</span>
-          <ul class="text-white flex flex-col gap-5 max-h-[500px] p-2 overflow-y-scroll">
-            <li class="p-2 bg-green-300 rounded "> list 1</li>
-            <li class="p-2 bg-green-300 rounded "> list 1</li>
-               <li class="p-2 bg-green-300 rounded "> list 1</li>
-            <li class="p-2 bg-green-300 rounded "> list 1</li>
-               <li class="p-2 bg-green-300 rounded "> list 1</li>
-            <li class="p-2 bg-green-300 rounded "> list 1</li>
-               <li class="p-2 bg-green-300 rounded "> list 1</li>
-            <li class="p-2 bg-green-300 rounded "> list 1</li>
-               <li class="p-2 bg-green-300 rounded "> list 1</li>
-            <li class="p-2 bg-green-300 rounded "> list 3</li>
+    <div id="list" class="bg-[#F1F2F4] min-w-[300px] max-h-[600px] ">
+          <span class="block list-title mb-3 pt-3 px-4 text-[#3F506C]"> ${this.state.list.title}</span>
+          <ul class="text-black flex flex-col gap-5 max-h-[450px] p-2 overflow-y-scroll">
+            <li class="p-2 bg-white rounded shadow-md "> list 1</li>
+            <li class="p-2 bg-white rounded shadow-md  "> list 1</li>
+               <li class="p-2 bg-white rounded shadow-md  "> list 1</li>
+            <li class="p-2 bg-white rounded shadow-md "> list 1</li>
+               <li class="p-2 bg-white rounded shadow-md  "> list 1</li>
+            <li class="p-2 bg-white rounded shadow-md "> list 1</li>
+               <li class="p-2 bg-white rounded shadow-md "> list 1</li>
+            <li class="p-2 bg-white rounded shadow-md "> list 1</li>
+               <li class="p-2 bg-white rounded shadow-md "> list 1</li>
+            <li class="p-2 bg-white rounded shadow-md  ">list 3</li>
 
           </ul>
+           <div class=" flex flex-col  gap-1 h-[100px] rounded-t-xl pl-2 pr-3 py-2 w-[300px] bg-[#F1F2F4] bg-red-100  "  >
+          <input id="new-list-input" class="p-3 rounded shadow-md"  placeholder="Add a new list" />
+          <button class="add-list bg-blue-400 h-[50px] hover:bg-blue-600 text-white rounded"> Add </button>
+        </div>
         </div>
     `;
 
     this.elements.parentEl.appendChild(listEl);
 
     listEl.classList.add(
-      "bg-blue-gray-500",
       "min-w-[300px]",
       "max-h-[600px]",
-      "rounded-3xl",
+      "rounded-2xl",
       "overflow-auto"
     );
 
